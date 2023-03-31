868601dbdb248fc6520ce634a13903e93fbcb066
Sarah Reimann <reimannsarah45@gmail.com>
2023-03-31 09:10:55 -0700

    WIP: working on branching logic for test 2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,20 @@
 
 function beepBoopNeighbor(number){
   let numberArray = [];
-  for(let i = 0; i <= number; i++)
-  numberArray.push(i).toString();
-  return numberArray;
+  for(let i = 0; i <= number; i++){
+  numberArray.push(i);
+  }
+  const beep = numberArray.map(function(digit){
+    if (digit === 1){
+        if(digit>9){
+          let digitArray = digit.toString().split("").map(Number);
+          console.log(digitArray);
+        }
+
+      return "Beep!";
+    }else{
+      return digit;
+    };
+  });
+  console.log(beep)
 }
